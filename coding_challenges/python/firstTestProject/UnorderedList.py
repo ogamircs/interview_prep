7f43a379ab2003ad271c8622e8196a033a04cb5d
Amir Tavasoli <amir.tavasoli@gmail.com>
2020-12-28 21:02:45 -0500

    data structures added

---
+++ coding_challenges/python/firstTestProject/UnorderedList.py
@@ -0,0 +1,31 @@
+from Node import Node
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.get_next()
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
--- coding_challenges/python/firstTestProject/main.py
+++ coding_challenges/python/firstTestProject/main.py
@@ -1,5 +1,9 @@
+from Deque import Deque
 from Fraction import Fraction
+from Queue import Queue
 from Stack import Stack
+from UnorderedList import UnorderedList
+
 
 def anagram_solution4(s1, s2):
     c1 = [0] * 26
@@ -56,6 +60,59 @@ def divide_by_2(dec_number, base):
 
     return bin_number
 
+def infix_to_postfix(infix_expr):
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    op_stack = Stack()
+    postfix_list = []
+    token_list = infix_expr.split()
+
+    for token in token_list:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfix_list.append(token)
+        elif token == '(':
+            op_stack.push(token)
+        elif token == ')':
+            top_token = op_stack.pop()
+            while top_token != '(':
+                postfix_list.append(top_token)
+                top_token = op_stack.pop()
+        else:
+            while (not op_stack.is_empty()) and \
+                    (prec[op_stack.peek()] >= prec[token]):
+                postfix_list.append(op_stack.pop())
+            op_stack.push(token)
+
+    while not op_stack.is_empty():
+        postfix_list.append(op_stack.pop())
+    return " ".join(postfix_list)
+
+def hot_potato(name_list, num):
+    sim_queue = Queue()
+    for name in name_list:
+        sim_queue.enqueue(name)
+    while sim_queue.size() > 1:
+        for i in range(num):
+            sim_queue.enqueue(sim_queue.dequeue())
+        sim_queue.dequeue()
+    return sim_queue.dequeue()
+
+def pal_checker(a_string):
+    char_deque = Deque()
+    for ch in a_string:
+        char_deque.add_rear(ch)
+
+    still_equal = True
+    while char_deque.size() > 1 and still_equal:
+        first = char_deque.remove_front()
+        last = char_deque.remove_rear()
+        if first != last:
+            still_equal = False
+    return still_equal
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -75,5 +132,36 @@ if __name__ == '__main__':
     print("binary_number:")
     print(divide_by_2(233, 2))
     print(divide_by_2(233, 16))
+    print("infix_to_postfix:")
+    print(infix_to_postfix("A * B + C * D"))
+    print(infix_to_postfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+    print("Queue:")
+    q = Queue()
+    q.enqueue('hello')
+    q.enqueue('dog')
+    q.enqueue(3)
+    print(q.dequeue())
+    print("Hot Potatoe:")
+    print(hot_potato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 3))
+    print("Deque:")
+    d = Deque()
+    d.add_rear(4)
+    d.add_rear('dog')
+    d.add_front(True)
+    print(d.remove_rear())
+    print(d.remove_front())
+    print("pal_checker")
+    print(pal_checker("radar"))
+    print(pal_checker("data"))
+    print("UnorderedList:")
+    mylist = UnorderedList()
+    mylist.add(2)
+    mylist.add(17)
+    s = mylist.size()
+    print(f"list size {s}")
+    print(mylist.search(17))
+
+
+
 
 
