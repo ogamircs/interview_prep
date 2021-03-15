d484a61d945fd3301f9aeb5627e54581240afbb4
Amir Tavasoli <amir.tavasoli@gmail.com>
2021-03-15 02:05:55 -0400

    test

---
+++ coding_challenges/python/leetcode/arrays/removeDuplicates.py
@@ -0,0 +1,24 @@
+
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    element = None
+    l = len(nums)
+    for i in range(0,l):
+        if(element != nums[i]):
+            element = nums[i]
+        elif(element == nums[i]):
+
+
+
+    return len(nums)
+
+if __name__ == '__main__':
+    nums = [1,1,2]
+    l = removeDuplicates(nums)
+    print(f"{l}, nums = {nums}")
+
+
+
