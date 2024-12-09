d0f114903425af5dcae7ef1428afd2a475fe8a0e
Amir Tavasoli <amir.tavasoli@gmail.com>
2024-12-08 21:45:28 -0500

    valid palindrome

--- coding_challenges/notebook/test.py
+++ coding_challenges/notebook/test.py
@@ -1,47 +1,30 @@
-def merge(nums1, m, nums2, n):
-    i = 0
-    j = 0
-    num_zeroes = n
+def validPalindrome(s):
+    L = len(s)
+    l = 0
+    r = L-1
+    skipped = False
+    while(l<r):
+        k = s[l]
+        kk = s[r]
+        if(s[l]==s[r]):
+            l += 1
+            r -= 1
+        elif((not skipped) and (s[l]!=s[r])):
+            skipped = True
+            if(s[l+1]==s[r]):
+                l += 1
+            elif(s[l]==s[r-1]):
+                r -= 1
+            else:
+                return False
+        elif(skipped and (s[l]!=s[r])):
+            return False
+    return True
 
-    if(n==0): 
-        return None
-    if(m==0): 
-        for i in range(n):
-            nums1[i] = nums2[i]
-        return None
-
-    while((num_zeroes>0) and (i<(m+n))):
-        if((i==0) and (nums1[i]>nums2[j])):
-            shiftForward(nums1, 1)
-            nums1[i] = nums2[j]
-            num_zeroes -= 1
-            j += 1
-
-        if((i>0) and (nums1[i]>nums2[j])):
-            shiftForward(nums1, i+1)
-            nums1[i] = nums2[j]
-            num_zeroes -= 1
-            j += 1
-        i += 1
-    
-    if(num_zeroes>0):
-        while((num_zeroes>0) and (i<(m+n))):
-            nums1[i] = nums2[j]
-            j += 1
-            i + 1
-            num_zeroes-=1
-    return None
-
-def shiftForward(nums, k):
-    i = len(nums)-1
-    while(i>=k):
-        nums[i], nums[i-1] = nums[i-1], nums[i]
-        i -= 1
-
-
-nums1 = [-1,3,0,0,0,0,0]
-m = 2
-nums2 = [0,0,1,2,3]
-n = 5
-merge(nums1, m, nums2, n)
-print(nums1)
+s = "aba"
+s = "abbca"
+#s = "abca"
+#s = "abc"
+#s = "abcf"
+s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"
+print(validPalindrome(s))
