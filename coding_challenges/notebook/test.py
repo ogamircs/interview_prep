7c40730eadaa811a55307f2000cbe54c953b6091
Amir Tavasoli <amir.tavasoli@gmail.com>
2025-01-05 22:19:31 -0500

    added dfs

--- coding_challenges/notebook/test.py
+++ coding_challenges/notebook/test.py
@@ -1,30 +1,31 @@
-def spiralArray(n):
-    def spiral(a):
-        n = len(a)
-        r = c = 0
-        for i in range(n*n):
-            a[r][c] = i+1
+from heapq import heappush, heappop
+from collections import deque
 
-            if(i+1<n):
-                c+=1
-            elif(i+1<(n*2)-1):
-                r+=1
-            elif(i+1<(n*3)-2):
-                c-=1
-            elif(i+1<(n*4)-4):
-                r-=1
-            else:
-                break
-            
-    arr = []
-    for i in range(n):
-        sarr = []
-        for j in range(n):
-            sarr.append(0)
-        arr.append(sarr)
-    
-    spiral(arr)
+def bfs(n, m, edges, s):
+    #build adjacency matrix
+    adj_matrix = {i: [] for i in range(0,n)}
+    for e in edges:
+        adj_matrix[e[0]].append(e[1])
+        adj_matrix[e[1]].append(e[0])
+    print(adj_matrix)
+    #now bfs through adj matrix
+    out = [-1] * n  # Distances array, -1 for unreachable nodes
+    visited = set()
+    queue = deque([s]) 
+    out[s - 1] = 0
+    # Step 3: Perform BFS
+    while queue:
+        current = queue.popleft()
+        visited.add(current)
 
-    return arr
+        # Traverse neighbors using a for loop
+        for neighbor in adj_matrix[current]:
+            if neighbor not in visited:  # Only visit unvisited nodes
+                visited.add(neighbor)
+                queue.append(neighbor)
+                out[neighbor - 1] = out[current - 1] + 6  # Increment distance by weight (6)
+    zero_ind = out.index(0)
+    return out[0:zero_ind]+out[zero_ind+1:n]
 
-print(spiralArray(3))
+
+bfs(5,6,[[0,1],[0,2],[1,2],[1,3],[2,4],[3,4]],0)
