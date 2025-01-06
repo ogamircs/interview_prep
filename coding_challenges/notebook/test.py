6e8083d4be431c12ad23e460e94875c2ff7f8d1e
Amir Tavasoli <amir.tavasoli@gmail.com>
2025-01-06 01:53:22 -0500

    added pairsum

--- coding_challenges/notebook/test.py
+++ coding_challenges/notebook/test.py
@@ -1,31 +1,36 @@
-from heapq import heappush, heappop
-from collections import deque
+def numberOfWays(arr,k):
+    n = len(arr)
+    res = 0
+    i = 0
+    j = n-1
+    a = sorted(arr)
+    while(i<j):
+        if((a[i]+a[j])==k):
+            if(a[i]==a[j]):
+                count = j - i + 1
+                res += (count * (count - 1)) // 2
+                break
+            else:
+                # Count duplicates for a[i] and a[j]
+                left_count = 1
+                right_count = 1
+                while i + 1 < j and a[i] == a[i + 1]:
+                    left_count += 1
+                    i += 1
+                while j - 1 > i and a[j] == a[j - 1]:
+                    right_count += 1
+                    j -= 1
+            res += left_count * right_count  # Total pairs for current i and j
+            i += 1
+            j -= 1
+        elif((a[i]+a[j])>k):
+            j -= 1
+        else:
+            i += 1
+        
+    return res
 
-def bfs(n, m, edges, s):
-    #build adjacency matrix
-    adj_matrix = {i: [] for i in range(0,n)}
-    for e in edges:
-        adj_matrix[e[0]].append(e[1])
-        adj_matrix[e[1]].append(e[0])
-    print(adj_matrix)
-    #now bfs through adj matrix
-    out = [-1] * n  # Distances array, -1 for unreachable nodes
-    visited = set()
-    queue = deque([s]) 
-    out[s - 1] = 0
-    # Step 3: Perform BFS
-    while queue:
-        current = queue.popleft()
-        visited.add(current)
-
-        # Traverse neighbors using a for loop
-        for neighbor in adj_matrix[current]:
-            if neighbor not in visited:  # Only visit unvisited nodes
-                visited.add(neighbor)
-                queue.append(neighbor)
-                out[neighbor - 1] = out[current - 1] + 6  # Increment distance by weight (6)
-    zero_ind = out.index(0)
-    return out[0:zero_ind]+out[zero_ind+1:n]
-
-
-bfs(5,6,[[0,1],[0,2],[1,2],[1,3],[2,4],[3,4]],0)
+arr = [1, 5, 3, 3, 3]
+arr = [1, 3, 3, 3, 5]
+k = 6
+print(numberOfWays(arr,k))
